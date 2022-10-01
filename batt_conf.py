e9c379e6e24a53a74401c50c1d6664cf22497499
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-10-01 01:29:44 -0500

    yes

--- batt_conf.py
+++ batt_conf.py
@@ -44,10 +44,13 @@ class config_t:
         self.total_voltage_max = series * pack_series * cell.volt_max
         self.typ_accumulator_capacity_wh = self.total_cells * cell.volt_nominal * cell.cap_typical_ah * 0.8
         self.typ_segment_capacity_mj = self.pack_cells * cell.volt_nominal * cell.cap_typical_ah * 0.0036
-        self.max_accumulator_capacity_wh = self.total_cells * cell.volt_max * cell.cap_typical_ah * 0.8
+        self.max_accumulator_capacity_wh = self.total_cells * cell.volt_max * cell.cap_typical_ah
         self.max_segment_capacity_mj = self.pack_cells * cell.volt_max * cell.cap_typical_ah * 0.0036
         self.segment_volt_nominal = series * cell.volt_nominal
         self.segment_volt_max = series * cell.volt_max
+
+    def __lt__(self, other) -> bool:
+        return self.max_accumulator_capacity_wh < other.max_accumulator_capacity_wh
 
     def __str__(self) -> str:
         params = {
@@ -67,46 +70,55 @@ class config_t:
             "max_accumulator_capacity_Wh": self.max_accumulator_capacity_wh,
             "max_segment_capacity_MJ": self.max_segment_capacity_mj,
         }
-        mystring = f"{self.cell.name}\n" + ' '.join(['\t' + k + " : " + str(v) + '\n' for k, v in params.items()])
+        v_dv = round(100*abs((self.total_voltage_max - VOLT_TARGET) / VOLT_TARGET), 4)
+        v_sign = '+' if VOLT_TARGET < self.total_voltage_max else '-'
+        v_sign = '+' if VOLT_TARGET < self.total_voltage_max else '-'
+        mystring = f"{VOLT_TARGET} {v_sign}{v_dv}% - {self.cell.name}\n"
+        mystring += ' '.join(['\t' + k + " : " + str(v) + '\n' for k, v in params.items()])
         return mystring
 
 # Cells
 
 
-P45B = cell_t("Molicel INR21700-P45B", 4.2, 3.6, 2.5, 4.5, 4.3, 45, 4.5, 66.8)
-P28B = cell_t("Molicel INR18650-P28B", 4.2, 3.6, 2.5, 2.8, 2.65, 40, 2.8, 45.4)
+S_50S = cell_t("Samsung INR21700-50S", 4.2, 3.6, 2.5, 5.0, 4.8, 45, 6, 72)
+P45B = cell_t("Molicel INR21700-P45B", 4.2, 3.6, 2.5, 4.5, 4.3, 45, 4.5, 70)
+P42A = cell_t("Molicel INR21700-P42A", 4.2, 3.6, 2.5, 4.2, 4.0, 45, 4.2, 70)
+P28B = cell_t("Molicel INR18650-P28B", 4.2, 3.6, 2.5, 2.8, 2.65, 40, 2.8, 48)
 
-CELLS = (P45B, P28B)
+CELLS = (P42A, P45B, S_50S, P28B)
 
 # Range Check
 RANGE_PACK_SERIES = range(1, 32)
 RANGE_PACK_PARALLEL = range(1, 32)
 RANGE_ACCUMULATOR_PACKS_SERIES = range(1, 12)
-RANGE_ACCUMULATOR_PACKS_PARALLEL = range(1, 12)
+RANGE_ACCUMULATOR_PACKS_PARALLEL = range(1, 2)
 
 # Targets
-VOLT_TARGET_MAX = 555
-VOLT_TARGET_MIN = 545
+VOLT_TARGET = 300
+VOLT_MARGIN = 20
 CAP_TOTAL_MAX_WH = 5405.5
 CAP_TOTAL_MIN_WH = CAP_TOTAL_MAX_WH * 0.7
 CAP_SEGMENT_MAX_MJ = 6.5
 VOLT_SEGMENT_MAX = 120.5
 
-# Config
-
-VOLT_TARGET_MAX = 355
-VOLT_TARGET_MIN = 345
+results = []
 
 for cell_sel in CELLS:
     for pk_parallel in RANGE_ACCUMULATOR_PACKS_PARALLEL:
         for pk_series in RANGE_ACCUMULATOR_PACKS_SERIES:
+            if pk_parallel * pk_series % 2 != 0:
+                continue
             for c_parallel in RANGE_PACK_PARALLEL:
-                if c_parallel % 2 != 0:
+                if c_parallel != 1 and c_parallel % 2 != 0:
                     continue
                 for c_series in RANGE_PACK_SERIES:
-                    if c_series % 2 != 0:
+                    if c_series != 1 and c_series % 2 != 0:
                         continue
                     conf = config_t(cell_sel, pk_parallel, pk_series, c_parallel, c_series)
-                    if VOLT_TARGET_MIN <= conf.total_voltage_max <= VOLT_TARGET_MAX and CAP_TOTAL_MIN_WH <= conf.typ_accumulator_capacity_wh <= CAP_TOTAL_MAX_WH:
+                    if VOLT_TARGET - VOLT_MARGIN <= conf.total_voltage_max <= VOLT_TARGET + VOLT_MARGIN and CAP_TOTAL_MIN_WH <= conf.typ_accumulator_capacity_wh <= CAP_TOTAL_MAX_WH:
                         if conf.segment_volt_max <= VOLT_SEGMENT_MAX and conf.typ_segment_capacity_mj <= CAP_SEGMENT_MAX_MJ:
-                            print(conf)
+                            results.append(conf)
+
+results.sort()
+for r in results:
+    print(r)
---
+++ configs.md
@@ -0,0 +1,109 @@
+# Battery Configs
+
+## 550V +0.8% - Molicel INR18650-P28B - Emrax 208
+
+total_cells : 528
+packs : 6
+pack_parallel : 1
+pack_series : 6
+parallel : 4
+series : 22
+weight (kg) : 25.344
+total_voltage_nominal : 475.2
+total_voltage_max : 554.4
+segment_volt_nominal : 79.2
+segment_volt_max : 92.4
+typ_accumulator_capacity_Wh : 4257.792
+typ_segment_capacity_MJ : 3.1933439999999997
+max_accumulator_capacity_Wh : 6209.28
+max_segment_capacity_MJ : 3.7255680000000004
+
+## 430V +1.58% - Molicel INR21700-P45B - Emrax 188
+
+total_cells : 416
+packs : 4
+pack_parallel : 1
+pack_series : 4
+parallel : 4
+series : 26
+weight (kg) : 29.12
+total_voltage_nominal : 374.40000000000003
+total_voltage_max : 436.8
+segment_volt_nominal : 93.60000000000001
+segment_volt_max : 109.2
+typ_accumulator_capacity_Wh : 5391.360000000001
+typ_segment_capacity_MJ : 6.0652800000000004
+max_accumulator_capacity_Wh : 7862.40000000001
+max_segment_capacity_MJ : 7.076160000000001
+
+## 350V +0.8% - Molicel INR21700-P45B - Emrax 208
+
+total_cells : 336
+packs : 6
+pack_parallel : 1
+pack_series : 6
+parallel : 4
+series : 14
+weight (kg) : 23.52
+total_voltage_nominal : 302.40000000000003
+total_voltage_max : 352.8
+segment_volt_nominal : 50.4
+segment_volt_max : 58.800000000000004
+typ_accumulator_capacity_Wh : 4354.56
+typ_segment_capacity_MJ : 3.2659199999999995
+max_accumulator_capacity_Wh : 6350.400000000001
+max_segment_capacity_MJ : 3.8102400000000003
+
+## 350V +0.8% - Samsung INR21700-50S ~80% CC - Emrax 208
+
+total_cells : 336
+packs : 6
+pack_parallel : 1
+pack_series : 6
+parallel : 4
+series : 14
+weight (kg) : 24.192
+total_voltage_nominal : 302.40000000000003
+total_voltage_max : 352.8
+segment_volt_nominal : 50.4
+segment_volt_max : 58.800000000000004
+typ_accumulator_capacity_Wh : 4838.400000000001
+typ_segment_capacity_MJ : 3.6288
+max_accumulator_capacity_Wh : 7056.0
+max_segment_capacity_MJ : 4.2336
+
+## 300V +0.8% - Molicel INR18650-P28B - Emrax 188
+
+total_cells : 576
+packs : 6
+pack_parallel : 1
+pack_series : 6
+parallel : 8
+series : 12
+weight (kg) : 27.648
+total_voltage_nominal : 259.2
+total_voltage_max : 302.40000000000003
+segment_volt_nominal : 43.2
+segment_volt_max : 50.400000000000006
+typ_accumulator_capacity_Wh : 4644.864
+typ_segment_capacity_MJ : 3.4836479999999996
+max_accumulator_capacity_Wh : 6773.75
+max_segment_capacity_MJ : 4.064256
+
+## 300 +0.8% - Molicel INR21700-P42A - Emrax 188
+
+total_cells : 432
+packs : 6
+pack_parallel : 1
+pack_series : 6
+parallel : 6
+series : 12
+weight (kg) : 30.24
+total_voltage_nominal : 259.2
+total_voltage_max : 302.40000000000003
+segment_volt_nominal : 43.2
+segment_volt_max : 50.400000000000006
+typ_accumulator_capacity_Wh : 5225.472000000001
+typ_segment_capacity_MJ : 3.9191040000000004
+max_accumulator_capacity_Wh : 7620.4800000000005
+max_segment_capacity_MJ : 4.572288
