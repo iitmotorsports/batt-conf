328cd768a3635f56aa3400d635cfdeddca8e6df4
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-10-04 19:16:36 -0500

    csv

--- batt_conf.py
+++ batt_conf.py
@@ -1,3 +1,7 @@
+import csv
+from typing import List
+
+
 class cell_t:
     name: str = None
     volt_max: float = 0
@@ -23,61 +27,63 @@ class cell_t:
 
 class config_t:
     total_cells = 0
-    total_voltage_nominal = 0
-    total_voltage_max = 0
-    typ_accumulator_capacity_wh = 0
-    typ_segment_capacity_mj = 0
-    max_accumulator_capacity_wh = 0
-    max_segment_capacity_mj = 0
+    total_volt_nominal = 0
+    total_volt_max = 0
+    total_typ_capacity_wh = 0
+    segment_typ_capacity_mj = 0
+    total_max_capacity_wh = 0
+    segment_max_capacity_mj = 0
 
-    def __init__(self, cell: cell_t, pack_parallel: float, pack_series: float, parallel: float, series: float) -> None:
+    def __init__(self, cell: cell_t, parallel_segments: float, series_segments: float, parallel: float, series: float) -> None:
         self.cell = cell
         self.parallel = parallel
         self.series = series
-        self.pack_parallel = pack_parallel
-        self.pack_series = pack_series
-        self.packs = pack_parallel * pack_series
-        self.pack_cells = parallel * series
-        self.total_cells = self.pack_cells * self.packs
+        self.parallel_segments = parallel_segments
+        self.series_segments = series_segments
+        self.segments = parallel_segments * series_segments
+        self.segment_cells = parallel * series
+        self.total_cells = self.segment_cells * self.segments
         self.weight = self.total_cells * cell.weight_gram
-        self.total_voltage_nominal = series * pack_series * cell.volt_nominal
-        self.total_voltage_max = series * pack_series * cell.volt_max
-        self.typ_accumulator_capacity_wh = self.total_cells * cell.volt_nominal * cell.cap_typical_ah * 0.8
-        self.typ_segment_capacity_mj = self.pack_cells * cell.volt_nominal * cell.cap_typical_ah * 0.0036
-        self.max_accumulator_capacity_wh = self.total_cells * cell.volt_max * cell.cap_typical_ah
-        self.max_segment_capacity_mj = self.pack_cells * cell.volt_max * cell.cap_typical_ah * 0.0036
+        self.total_volt_nominal = series * series_segments * cell.volt_nominal
+        self.total_volt_max = series * series_segments * cell.volt_max
+        self.total_typ_capacity_wh = self.total_cells * cell.volt_nominal * cell.cap_typical_ah * 0.8
+        self.segment_typ_capacity_mj = self.segment_cells * cell.volt_nominal * cell.cap_typical_ah * 0.0036
+        self.total_max_capacity_wh = self.total_cells * cell.volt_max * cell.cap_typical_ah
+        self.segment_max_capacity_mj = self.segment_cells * cell.volt_max * cell.cap_typical_ah * 0.0036
         self.segment_volt_nominal = series * cell.volt_nominal
         self.segment_volt_max = series * cell.volt_max
 
     def info(self) -> dict:
-        v_sign = '+' if VOLT_TARGET < self.total_voltage_max else '-'
-        v_sign = '+' if VOLT_TARGET < self.total_voltage_max else '-'
-        v_dv = round(100*abs((self.total_voltage_max - VOLT_TARGET) / VOLT_TARGET), 4)
+        v_sign = '+' if VOLT_TARGET < self.total_volt_max else '-'
+        v_sign = '+' if VOLT_TARGET < self.total_volt_max else '-'
+        v_dv = round(100*abs((self.total_volt_max - VOLT_TARGET) / VOLT_TARGET), 4)
         return {
+            "cell": self.cell.name,
             "target_volt_dv": f"{v_sign}{v_dv}",
             "total_cells": self.total_cells,
-            "packs": self.packs,
-            "pack_parallel": self.pack_parallel,
-            "pack_series": self.pack_series,
+            "segments": self.segments,
+            "parallel_segments": self.parallel_segments,
+            "series_segments": self.series_segments,
             "parallel": self.parallel,
             "series": self.series,
-            "weight (kg)": self.weight/1000,
-            "total_voltage_nominal": self.total_voltage_nominal,
-            "total_voltage_max": self.total_voltage_max,
+            "weight_kg": self.weight/1000,
+            "total_volt_nominal": self.total_volt_nominal,
+            "total_volt_max": self.total_volt_max,
+            "total_typ_capacity_wh": self.total_typ_capacity_wh,
+            "total_max_capacity_wh": self.total_max_capacity_wh,
             "segment_volt_nominal": self.segment_volt_nominal,
             "segment_volt_max": self.segment_volt_max,
-            "typ_accumulator_capacity_Wh": self.typ_accumulator_capacity_wh,
-            "typ_segment_capacity_MJ": self.typ_segment_capacity_mj,
-            "max_accumulator_capacity_Wh": self.max_accumulator_capacity_wh,
-            "max_segment_capacity_MJ": self.max_segment_capacity_mj,
+            "segment_typ_capacity_mj": self.segment_typ_capacity_mj,
+            "segment_max_capacity_mj": self.segment_max_capacity_mj,
         }
 
     def __lt__(self, other) -> bool:
-        return self.max_accumulator_capacity_wh < other.max_accumulator_capacity_wh
+        return self.total_max_capacity_wh < other.total_max_capacity_wh
 
     def __str__(self) -> str:
         params = self.info()
         v_dv = params["target_volt_dv"]
+        del params["cell"]
         del params["target_volt_dv"]
         mystring = f"{VOLT_TARGET}V {v_dv}% - {self.cell.name}\n"
         mystring += ' '.join(['\t' + k + " : " + str(v) + '\n' for k, v in params.items()])
@@ -93,10 +99,10 @@ P28B = cell_t("Molicel INR18650-P28B", 4.2, 3.6, 2.5, 2.8, 2.65, 40, 2.8, 48)
 CELLS = (P42A, P45B, S_50S, P28B)
 
 # Range Check
-RANGE_PACK_SERIES = range(1, 32)
-RANGE_PACK_PARALLEL = range(1, 32)
-RANGE_ACCUMULATOR_PACKS_SERIES = range(1, 12)
-RANGE_ACCUMULATOR_PACKS_PARALLEL = range(1, 4)
+RANGE_SEGMENT_SERIES = range(1, 64)
+RANGE_SEGMENT_PARALLEL = range(1, 32)
+RANGE_ACCUMULATOR_SEGMENTS_SERIES = range(1, 64)
+RANGE_ACCUMULATOR_SEGMENTS_PARALLEL = range(1, 3)
 
 # Targets
 VOLT_TARGET = 550
@@ -106,24 +112,25 @@ CAP_TOTAL_MIN_WH = CAP_TOTAL_MAX_WH * 0.6
 CAP_SEGMENT_MAX_MJ = 6.5
 VOLT_SEGMENT_MAX = 120.5
 
-results = []
+results: List[config_t] = []
 
 for cell_sel in CELLS:
-    for pk_parallel in RANGE_ACCUMULATOR_PACKS_PARALLEL:
-        for pk_series in RANGE_ACCUMULATOR_PACKS_SERIES:
+    for pk_parallel in RANGE_ACCUMULATOR_SEGMENTS_PARALLEL:
+        for pk_series in RANGE_ACCUMULATOR_SEGMENTS_SERIES:
             if pk_parallel * pk_series % 2 != 0:
                 continue
-            for c_parallel in RANGE_PACK_PARALLEL:
+            for c_parallel in RANGE_SEGMENT_PARALLEL:
                 if c_parallel != 1 and c_parallel % 2 != 0:
                     continue
-                for c_series in RANGE_PACK_SERIES:
+                for c_series in RANGE_SEGMENT_SERIES:
                     if c_series != 1 and c_series % 2 != 0:
                         continue
                     conf = config_t(cell_sel, pk_parallel, pk_series, c_parallel, c_series)
-                    if VOLT_TARGET - VOLT_MARGIN <= conf.total_voltage_max <= VOLT_TARGET + VOLT_MARGIN and CAP_TOTAL_MIN_WH <= conf.typ_accumulator_capacity_wh <= CAP_TOTAL_MAX_WH:
-                        if conf.segment_volt_max <= VOLT_SEGMENT_MAX and conf.typ_segment_capacity_mj <= CAP_SEGMENT_MAX_MJ:
+                    if VOLT_TARGET - VOLT_MARGIN <= conf.total_volt_max <= VOLT_TARGET + VOLT_MARGIN and CAP_TOTAL_MIN_WH <= conf.total_typ_capacity_wh <= CAP_TOTAL_MAX_WH:
+                        if conf.segment_volt_max <= VOLT_SEGMENT_MAX and conf.segment_typ_capacity_mj <= CAP_SEGMENT_MAX_MJ:
                             results.append(conf)
 
-results.sort()
-for r in results:
-    print(r)
+with open("output.csv", 'w', encoding='utf-8') as csvfile:
+    f = csv.writer(csvfile, dialect='excel')
+    f.writerow(results[0].info().keys())
+    f.writerows([x.info().values() for x in results])
