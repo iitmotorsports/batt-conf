fbc6d8278ff7f57353d6d6aa3e529d23a8c75a70
SAE <sae@iit.edu>
2022-11-02 17:53:49 -0500

    better size calc

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from cell import cell_t
 
-CELL_DIAMETER_BUF_MM = 1.8
+CELL_DIAMETER_BUF_MM = 2
 
 
 class config_t:
@@ -26,10 +26,12 @@ class config_t:
         self.segment_volt_nominal = series * cell.volt_nominal
         self.segment_volt_max = series * cell.volt_max
         self.voltage_target = voltage_target
-        self.segment_width = (0.866 * (self.parallel - 1) + 1) * (cell.diameter + CELL_DIAMETER_BUF_MM) # TODO: dim calc is a bit off?
-        self.segment_length = ((self.segment_cells / self.parallel) + 0.5) * (cell.diameter + CELL_DIAMETER_BUF_MM)
+        # TODO: dim calc is a bit off?
+        self.segment_width = (self.parallel + 0.5) * (cell.diameter + CELL_DIAMETER_BUF_MM)
+        self.segment_length = (0.866 * ((self.segment_cells / self.parallel)-1) + 1) * (cell.diameter + CELL_DIAMETER_BUF_MM)
         self.segment_dim = f"{int(self.segment_length)}mm x {int(self.segment_width)}mm"
         self.segment_m_2 = round(self.segment_length * self.segment_width / 1000000, 3)
+        self.volume_m_3 = round(self.segment_length * self.segment_width * cell.height * self.segments / 1000000000, 3)
 
     def info(self) -> dict:
         v_sign = '+' if self.voltage_target < self.total_volt_max else '-'
@@ -44,7 +46,8 @@ class config_t:
             # "series_segments": self.series_segments,
             "Seg Parallel": self.parallel,
             "Seg Series": self.series,
-            "Weight Kg": self.weight / 1000,
+            "Kg": round(self.weight / 1000, 2),
+            "m³": self.volume_m_3,
             "Volt Nom": self.total_volt_nominal,
             "Volt Max": self.total_volt_max,
             "Power Nom kW": self.total_discharge_cont_amps * self.total_volt_nominal / 1000,  # TODO: How is power calculated?
