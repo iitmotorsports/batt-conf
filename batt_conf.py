9f4fcda947882509aba01b8c5c21ceffaa8fb450
LeHuman <47549331+LeHuman@users.noreply.github.com>
2023-01-17 02:50:49 -0600

    update calculated

--- batt_conf.py
+++ batt_conf.py
@@ -4,18 +4,19 @@ from cell import CELLS
 from calc import config_t
 
 # Range Check
-RANGE_SEGMENT_SERIES = range(1, 65)
+RANGE_SEGMENT_SERIES = range(1, 25)
 RANGE_SEGMENT_PARALLEL = range(1, 65)
-RANGE_ACCUMULATOR_SEGMENTS_SERIES = range(1, 17)
+RANGE_ACCUMULATOR_SEGMENTS_SERIES = range(1, 13)
 RANGE_ACCUMULATOR_SEGMENTS_PARALLEL = range(1, 2)
 
 # Targets
-VOLT_TARGET = 350
-VOLT_MARGIN = 50
+VOLT_TARGET = 390
+VOLT_MAX = 600
+VOLT_MIN = 350
 CAP_TOTAL_MAX_WH = 7000
 CAP_TOTAL_MIN_WH = 3000
-CAP_SEGMENT_MAX_MJ = 6.5
-VOLT_SEGMENT_MAX = 120.5
+CAP_SEGMENT_MAX_MJ = 6.1
+VOLT_SEGMENT_MAX = 120.1
 
 results: List[config_t] = []
 
@@ -31,7 +32,7 @@ for cell_sel in CELLS:
                     if c_series != 1 and c_series % 2 != 0:
                         continue
                     conf = config_t(cell_sel, pk_parallel, pk_series, c_parallel, c_series, VOLT_TARGET)
-                    if VOLT_TARGET - VOLT_MARGIN <= conf.total_volt_nominal <= VOLT_TARGET + VOLT_MARGIN and CAP_TOTAL_MIN_WH <= conf.total_typ_capacity_wh <= CAP_TOTAL_MAX_WH:
+                    if VOLT_MIN <= conf.total_volt_nominal <= VOLT_MAX and CAP_TOTAL_MIN_WH <= conf.total_typ_capacity_wh <= CAP_TOTAL_MAX_WH:
                         if conf.segment_volt_max <= VOLT_SEGMENT_MAX and conf.segment_typ_capacity_mj <= CAP_SEGMENT_MAX_MJ:
                             results.append(conf)
 
--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from cell import cell_t
+from math import pi
 
 CELL_DIAMETER_BUF_MM = 2  # Approx 4mm of pcc between cells
 PCC_DENSITY_KG_M_3 = 890
@@ -15,12 +16,14 @@ class config_t:
         self.segments = parallel_segments * series_segments
         self.segment_cells = parallel * series
         self.total_cells = self.segment_cells * self.segments
-        self.weight = self.total_cells * cell.weight_gram
+        self.weight_kg = self.total_cells * cell.weight_gram / 1000
         self.total_volt_nominal = series * series_segments * cell.volt_nominal
         self.total_volt_max = series * series_segments * cell.volt_max
         self.total_typ_capacity_wh = self.total_cells * cell.volt_nominal * cell.cap_typical_ah * 0.8
         self.total_discharge_cont_amps = cell.discharge_cont_amps * self.parallel
         self.total_discharge_peak_amps = cell.discharge_peak_amps * self.parallel
+        self.power_nom_kw = self.total_discharge_cont_amps * self.total_volt_nominal / 1000  # TODO: How is power calculated?
+        self.power_max_kw = self.total_discharge_peak_amps * self.total_volt_max / 1000
         self.segment_typ_capacity_mj = self.segment_cells * cell.volt_nominal * cell.cap_typical_ah * 0.0036
         self.total_max_capacity_wh = self.total_cells * cell.volt_max * cell.cap_typical_ah
         self.segment_max_capacity_mj = self.segment_cells * cell.volt_max * cell.cap_typical_ah * 0.0036
@@ -31,15 +34,13 @@ class config_t:
         self.segment_width = (self.parallel + 0.5) * (cell.diameter + CELL_DIAMETER_BUF_MM)
         self.segment_length = (0.866 * ((self.segment_cells / self.parallel)-1) + 1) * (cell.diameter + CELL_DIAMETER_BUF_MM)
         self.segment_dim = f"{int(self.segment_length)}mm x {int(self.segment_width)}mm"
-        self.segment_m_2 = round(self.segment_length * self.segment_width / 1000000, 3)
-        self.volume_m_3 = round(self.segment_length * self.segment_width * cell.height * self.segments / 1000000000, 4)
+        self.segment_m_2 = self.segment_length * self.segment_width / 1000000
+        self.volume_m_3 = self.segment_length * self.segment_width * cell.height * self.segments / 1000000000
         self.pcc_weight_kg = 0
         if PCC_DENSITY_KG_M_3:
-            segment_width = (self.parallel + 0.5) * cell.diameter
-            segment_length = (0.866 * ((self.segment_cells / self.parallel)-1) + 1) * cell.diameter
-            volume_m_3 = round(segment_length * segment_width * cell.height * self.segments / 1000000000, 4)
-            self.pcc_weight_kg = (self.volume_m_3 - volume_m_3) * PCC_DENSITY_KG_M_3
-            self.weight += self.pcc_weight_kg * 1000
+            cell_volume_m_3 = self.total_cells * pi * ((cell.diameter / 2 / 1000) ** 2) * (cell.height / 1000)
+            self.pcc_weight_kg = (self.volume_m_3 - cell_volume_m_3) * PCC_DENSITY_KG_M_3
+            self.weight_kg += self.pcc_weight_kg
         self.price = round(cell.price * self.total_cells, 2)
 
     def info(self) -> dict:
@@ -51,24 +52,26 @@ class config_t:
             "V∆": f"{v_sign}{v_dv}",
             "Cells": self.total_cells,
             "Price": self.price,
-            "Segments": self.segments,
-            # "Parallel Seg": self.parallel_segments,
-            # "Series Seg": self.series_segments,
+            "Total Segments": self.segments,
+            "Parallel Seg": self.parallel_segments,
+            "Series Seg": self.series_segments,
             "Parallel Seg Cell": self.parallel,
             "Series Seg Cell": self.series,
-            "Kg": round(self.weight / 1000, 2),
+            "Kg": round(self.weight_kg, 2),
             "PCC Kg": round(self.pcc_weight_kg, 2),
-            "m³": self.volume_m_3,
+            "m³": round(self.volume_m_3, 4),
             "Volt Nom": self.total_volt_nominal,
             "Volt Max": self.total_volt_max,
-            "Power Nom kW": self.total_discharge_cont_amps * self.total_volt_nominal / 1000,  # TODO: How is power calculated?
-            "Power Max kW": self.total_discharge_peak_amps * self.total_volt_max / 1000,
+            "Power Nom kW": self.power_nom_kw,
+            "Power Max kW": self.power_max_kw,
             "Typ Cap Wh": self.total_typ_capacity_wh,
             "Max Cap Wh": self.total_max_capacity_wh,
+            "Wh/Kg": round(self.total_typ_capacity_wh / self.weight_kg, 4),
+            "kW/Kg": round(self.power_nom_kw / self.weight_kg, 2),
             "Discharge Cont. A": self.total_discharge_cont_amps,
             "Discharge Peak A": self.total_discharge_peak_amps,
             "Seg Dim": self.segment_dim,
-            "Seg m²": self.segment_m_2,
+            "Seg m²": round(self.segment_m_2, 2),
             "Seg Volt Nom": self.segment_volt_nominal,
             "Seg Volt Max": self.segment_volt_max,
             "Seg Typ Cap MJ": self.segment_typ_capacity_mj,
